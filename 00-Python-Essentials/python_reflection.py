7dc014eeed108436676d5e6e901fe056c1433391
hiyongz <zhiyo2016@163.com>
2021-06-01 07:42:51 +0800

    update Python reflection

--- 00-Python-Essentials/python_reflection.py
+++ 00-Python-Essentials/python_reflection.py
@@ -20,22 +20,38 @@ class Person():
 
 p = Person(20, 180)
 print(p)
-print(p.__str__)
-# print(p.__dict__)
-# p.__dict__['age']=22
-# print(p.__dict__)
+print(p.__dict__)
+p.__dict__['age']=22
+print(p.__dict__)
 p.weight = 60
-# print(p.__dict__)
-# print(dir(p))
+print(p.__dict__)
+print(dir(p))
 
 if (callable(p)):
     print("p is callable")
 else:
     print("p is not callable")
 
+#
+print(isinstance(p, Person))
+print(type(p) == Person)
+print(isinstance(p.age, int))
+print(type(p.age) == int)
+
+#
 print(hasattr(p,"talk"))
 print(hasattr(p.talk,"__call__"))
 
-print(isinstance(p, Person))
-print(isinstance(p.age, int))
+print(getattr(p,"talk"))
+print(getattr(p.talk, "__call__"))
 
+if hasattr(p,'walk'):
+    print(getattr(p,'show'))
+else:
+    print("I can't walk")
+
+setattr(p,'walk','ON')
+if hasattr(p,'walk'):
+    print(getattr(p,'show'))
+else:
+    print("I can't walk")
