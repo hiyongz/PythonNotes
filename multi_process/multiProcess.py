8f5b76d0db26b6ff4801098757f4bf970088a449
hiyongz <zhiyo2016@163.com>
2021-06-13 22:38:16 +0800

    update 多进程和多线程

--- multi_process/multiProcess.py
+++ multi_process/multiProcess.py
@@ -7,6 +7,7 @@
 # python 多进程
 
 from multiprocessing import Process
+from multiprocessing import Pool, cpu_count
 import os
 import time
 from multi_process.mytask import Task
@@ -36,6 +37,33 @@ class TestProcess():
         p2.join()
         end = time.time()
         print("耗时{}秒".format((end - start)))
+
+    def multiprocess_cpu_bound2(self):
+        # https://zhuanlan.zhihu.com/p/46368084
+        # Pool类创建多进程
+        print(f"CPU内核数:{cpu_count()}")
+        print(f'父进程: {os.getpid()}')
+        start = time.time()
+        p = Pool(4)
+        for i in range(2):
+            p.apply_async(Task.cpu_bound_task, args=(self,100000000,i))
+        p.close()
+        p.join()
+        end = time.time()
+        print(f"耗时{end - start}秒")
+    def multiprocess_io_bound2(self):
+        # Pool类创建多进程
+        print(f"CPU内核数:{cpu_count()}")
+        print(f'父进程: {os.getpid()}')
+        start = time.time()
+        p = Pool(8)
+        for i in range(2):
+            p.apply_async(Task.io_bound_task, args=(self,4, i+1))
+        p.close()
+        p.join()
+        end = time.time()
+        print(f"耗时{end - start}秒")
+
 
 if __name__=='__main__':
     proce = TestProcess()
--- multi_process/multithreading.py
+++ multi_process/multithreading.py
@@ -9,12 +9,11 @@
 import os
 import threading
 import time
-from multiprocessing import Pool, cpu_count
 from multi_process.mytask import Task
 
 class TestThread():
 
-    def multiprocess_cpu_bound(self):
+    def multithread_cpu_bound(self):
         start = time.time()
         print(f'主线程: {os.getpid()}')
         thread_list = []
@@ -31,22 +30,10 @@ class TestThread():
         end = time.time()
         print(f"耗时{end - start}秒")
 
-    def multiprocess_cpu_bound2(self):
-        # https://zhuanlan.zhihu.com/p/46368084
-        # Pool类创建多进程
-        print(f"CPU内核数:{cpu_count()}")
-        print(f'父进程: {os.getpid()}')
-        start = time.time()
-        p = Pool(4)
-        for i in range(2):
-            p.apply_async(Task.cpu_bound_task, args=(self,100000000,i))
-        p.close()
-        p.join()
-        end = time.time()
-        print(f"耗时{end - start}秒")
 
 
-    def multiprocess_io_bound(self):
+
+    def multithread_io_bound(self):
         start = time.time()
         print(f'主线程: {os.getpid()}')
         thread_list = []
@@ -63,22 +50,10 @@ class TestThread():
         end = time.time()
         print(f"耗时{end - start}秒")
 
-    def multiprocess_io_bound2(self):
-        # Pool类创建多进程
-        print(f"CPU内核数:{cpu_count()}")
-        print(f'父进程: {os.getpid()}')
-        start = time.time()
-        p = Pool(8)
-        for i in range(2):
-            p.apply_async(Task.io_bound_task, args=(self,4, i+1))
-        p.close()
-        p.join()
-        end = time.time()
-        print(f"耗时{end - start}秒")
+
 
 if __name__=='__main__':
     proce = TestThread()
-    proce.multiprocess_cpu_bound()
-    proce.multiprocess_cpu_bound2()
-    proce.multiprocess_io_bound()
-    proce.multiprocess_io_bound2()
+    proce.multithread_cpu_bound()
+    proce.multithread_io_bound()
+
