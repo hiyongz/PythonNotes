7fa7335d1453d54f37b5cdb05943cf66199c11fc
hiyongz <zhiyo2016@163.com>
2021-06-18 17:50:15 +0800

    add passing_arguments.ipynb update test_click.py

--- Parameter-Parsing/test_click.py
+++ Parameter-Parsing/test_click.py
@@ -3,15 +3,15 @@
 # @Time:    2021/6/17 15:31
 # @Author:  haiyong
 # @File:    test_click.py
-import string
 
 import click
 
 @click.command()
-@click.option('--field', '-f', help='字段')
-# @click.option('--display-filter', '-Y', prompt='display-filter', nargs=2, help='条件')
-@click.option('--display-filter', '-Y', prompt='display-filter', type=(str, int, float, bool), help='条件')
-@click.option('--count', '-c', default=2, prompt='count', help='条件')
+@click.option('--field', '-f', type=str, help='字段', multiple=True)
+@click.option('--display-filter', '-Y', prompt='display-filter', help='条件')
+# @click.option('--display-filter', '-Y', prompt='display-filter', type=(str, int, float, bool), help='条件')
+@click.option('--count', '-c', default=2, prompt='count', help='计数', count=True)
+# @click.help_option('--help', '-h', help='帮助信息')
 def cli(field, display_filter,count):
     """Simple program that greets NAME for a total of COUNT times."""
 
