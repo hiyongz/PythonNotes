ad146eafa0ffab43ac227f1a0fc8c9dcab6d41b5
hiyongz <zhiyo2016@163.com>
2021-11-04 19:44:12 +0800

    update test_folder.py

--- 03-File-Handling/test_folder.py
+++ 03-File-Handling/test_folder.py
@@ -14,3 +14,10 @@ print(os.path.realpath(__file__))
 print(os.path.dirname(os.path.realpath(__file__)))
 # 获取当前文件__file__的所在目录
 print(os.path.split(os.path.realpath(__file__))[0])
+
+# path = os.path.dirname(os.path.realpath(__file__)).decode('gbk')
+# tmpdir = os.path.join(path, "tmp")
+# staticdir = os.path.join(path, "static")
+# image_one = os.path.join(tmpdir, self.im_source_path)
+# image_two = os.path.join(staticdir, self.im_target_path)
+# im_source_obj = Image.open(self.im_source_path)
