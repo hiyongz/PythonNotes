0b035449dac320f15df40d37ce222ef3dc295418
hiyongz <zhiyo2016@163.com>
2021-12-01 07:44:27 +0800

    update pytesseract_demo.py

--- 09-Tools/pytesseract_demo.py
+++ 09-Tools/pytesseract_demo.py
@@ -3,6 +3,7 @@
 # @Time:    2021/11/27 17:47
 # @Author:  haiyong
 # @File:    pytesseract_demo.py
+import re
 
 import pytesseract
 try:
@@ -10,12 +11,26 @@ try:
 except ImportError:
     import Image
 
-# 列出支持的语言
-print(pytesseract.get_languages(config=''))
+class TesseractDemo():
+    def demo(self):
+        # 列出支持的语言
+        print(pytesseract.get_languages(config=''))
 
-# If you don't have tesseract executable in your PATH, include the following:
-# pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+        # If you don't have tesseract executable in your PATH, include the following:
+        # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+        # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
-# Simple image to string
-print(pytesseract.image_to_string(Image.open('test.png'), lang='chi_sim+eng'))
+        # Simple image to string
+        print(pytesseract.image_to_string(Image.open('test.png'), lang='chi_sim+eng'))
+
+    def osd_demo(self):
+        img = Image.open('osd-example.png')
+        osd = pytesseract.image_to_osd(img)
+        angle = re.search('(?<=Rotate: )\d+', osd).group(0)
+        script = re.search('(?<=Script: )\d+', osd).group(0)
+        print("angle: ", angle)
+        print("script: ", script)
+
+if __name__ == '__main__':
+    tes = TesseractDemo()
+    tes.osd_demo()
