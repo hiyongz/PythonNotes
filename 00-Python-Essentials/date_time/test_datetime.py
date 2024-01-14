1998d1c6086bf6918eec4f741a261011e6cb85fd
hiyongz <zhiyo2016@163.com>
2024-01-14 21:22:47 +0800

    【datetime】更新datetime笔记

--- 00-Python-Essentials/date_time/test_datetime.py
+++ 00-Python-Essentials/date_time/test_datetime.py
@@ -4,14 +4,28 @@
 # @Author:  haiyong
 # @File:    test_date.py
 import datetime
+from dateutil import parser
+from dateutil import rrule
 import os
 
 class testDateTime():
     def init_arg_s(self,init_kargs,kargs):
-        for k,v in kargs.iteritems():
-            if init_kargs.has_key(k):
+        for k,v in kargs.items():
+            if k in init_kargs:
                 init_kargs[k] = v
         return init_kargs
+
+
+    def adb_get_time(self):
+        time_local = os.popen('adb shell date "+%Y-%m-%d %H:%M:%S"')
+        time_local = time_local.read()
+        time_local = time_local.strip()
+        print("nowdate: ",time_local)
+        dates = time_local.split(' ')[0]
+        times = time_local.split(' ')[1]
+        # time_y = os.popen('adb shell date "+%Y').read().strip()
+        # time_m = os.popen('adb shell date "+%m').read().strip()
+        # time_d = os.popen('adb shell date "+%d').read().strip()
 
     def add_datetime(self, kargs='{}'):
         kargs = eval(kargs)
@@ -30,15 +44,7 @@ class testDateTime():
         ##### 获取手机时间 #####
         # now_time_new = datetime.datetime.now().strftime('%H:%M')
         # print "now_time_new",self.now_time_new
-        time_local = os.popen('adb shell date "+%Y-%m-%d %H:%M:%S"')
-        time_local = time_local.read()
-        time_local = time_local.strip()
-        print "nowdate: ",time_local
-        dates = time_local.split(' ')[0]
-        times = time_local.split(' ')[1]
-        # time_y = os.popen('adb shell date "+%Y').read().strip()
-        # time_m = os.popen('adb shell date "+%m').read().strip()
-        # time_d = os.popen('adb shell date "+%d').read().strip()
+        time_local = "2024-01-15 20:29:00"
 
         ##### 天数增加减少 #####
         # time_local = strTime
@@ -76,8 +82,42 @@ class testDateTime():
             resulttime = time_local[0:10]
             return resulttime
 
+    def time_offset(self):
+        d1 = '2024-01-01 20:29:00'
+        d2 = '2024-02-15 20:29:02'
+        # 将字符串转化为datetime格式
+        # date1 = datetime.datetime.strptime(d1, "%Y-%m-%d").date()  ##datetime.date(2018, 1, 6)
+        date1 = datetime.datetime.strptime(d1, "%Y-%m-%d %H:%M:%S")  ##datetime.date(2018, 1, 6)
+        # date2 = datetime.datetime.strptime(d2, "%Y-%m-%d").date()  ##datetime.date(2018, 1, 9)
+        date2 = datetime.datetime.strptime(d2, "%Y-%m-%d %H:%M:%S")  ##datetime.date(2018, 1, 9)
+        # 天数差
+        Days = (date2 - date1).days        
+        print(Days)
+        Days = (parser.parse(d2) - parser.parse(d1)).days
+        print(Days)
+        Days = rrule.rrule(rrule.DAILY, dtstart = date1, until = date2).count()
+        print(Days)
+        # 秒数差 
+        Seconds = (date2 - date1).total_seconds  
+        print(Seconds)
+        # 毫秒数
+        Microseconds = (date2 - date1).microseconds  
+        print(Microseconds)
+        # 周数差（rrule）
+        Weeks = rrule.rrule(rrule.WEEKLY, dtstart = date1, until = date2).count() 
+        print(Weeks)
+        # 月数差（rrule）        
+        Months = rrule.rrule(rrule.MONTHLY, dtstart = date1, until = date2).count()  
+        print(Months)        
+        # 年数差
+        Years = rrule.rrule(rrule.YEARLY, dtstart = date1, until = date2).count()  
+        print(Years)
+
+
+
 
 
 Date = testDateTime()
 nowdate = Date.add_datetime(str({"addtime_d": "6"}))
-print nowdate
+print(nowdate)
+Date.time_offset()
